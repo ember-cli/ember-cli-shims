0f5d27351e7ed6b732890eca75092ce6aa703d1e
Robert Jackson <robert.w.jackson@me.com>
2014-09-05 14:59:25 -0400

    Add commonly used modules.

--- app-shims.js
+++ app-shims.js
@@ -3,15 +3,78 @@
 
   var shims = {
     'ember': {
-      'default': Ember
+      'default': Ember,
+      'get': Ember.get,
+      'set': Ember.set
     },
     'ember/computed': {
       'default': Ember.computed
+    },
+    'ember/route': {
+      'default': Ember.Route
+    },
+    'ember/router': {
+      'default': Ember.Router
+    },
+    'ember/controller': {
+      'default': Ember.Controller
+    },
+    'ember/controllers/array': {
+      'default': Ember.ArrayController
+    },
+    'ember/controllers/object': {
+      'default': Ember.ObjectController
+    },
+    'ember/component': {
+      'default': Ember.Component
+    },
+    'ember/view': {
+      'default': Ember.View
+    },
+    'ember/views/container': {
+      'default': Ember.ContainerView
+    },
+    'ember/mixin': {
+      'default': Ember.Mixin
+    },
+    'ember/object': {
+      'default': Ember.Object
     },
 
     'ember-data': {
       'default': DS
     },
+    'ember-data/model': {
+      'default': DS.Model
+    },
+    'ember-data/serializers/rest': {
+      'default': DS.RESTSerializer
+    },
+    'ember-data/serializers/active-model': {
+      'default': DS.ActiveModelSerializer
+    },
+    'ember-data/serializers/json': {
+      'default': DS.JSONSerializer
+    },
+    'ember-data/adapters/rest': {
+      'default': DS.RESTAdapter
+    },
+    'ember-data/adapter': {
+      'default': DS.Adapter
+    },
+    'ember-data/adapters/active-model': {
+      'default': DS.ActiveModelAdapter
+    },
+    'ember-data/serializers/json': {
+      'default': DS.JSONSerializer
+    },
+    'ember-data/store': {
+      'default': DS.Store
+    },
+    'ember-data/transform': {
+      'default': DS.Transform
+    },
+
     'jquery': {
       'default': jQuery
     }
