1b318446238c144584ce4122fd81b4c1a6a5b3b0
Doug Yun <yun.douglas@gmail.com>
2016-03-04 15:10:15 -0500

    Add K as a `ember-utils`

--- app-shims.js
+++ app-shims.js
@@ -183,6 +183,7 @@
         'w':            Ember.String.w
       },
       'ember-utils': {
+        'K':          Ember.K,
         'isBlank':    Ember.isBlank,
         'isEmpty':    Ember.isEmpty,
         'isNone':     Ember.isNone,
