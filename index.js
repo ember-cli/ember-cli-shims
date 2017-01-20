4af3a0e12c46e5ac8f8562f38d11f90337648b41
Robert Jackson <rjackson@linkedin.com>
2017-01-20 18:59:45 -0500

    Fix for Node 0.12 :(
    ember-cli@2.11.0 intends to support Node 0.12, and this addon is
    needed in ember-cli@2.11.0.  This makes me sad.

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
 /* eslint-env node */
 'use strict';
 
-const SilentError = require('silent-error');
+var SilentError = require('silent-error');
 
 module.exports = {
   name: 'ember-cli-shims',
 
-  included(app) {
+  included: function(app) {
     this._super.included.apply(this, arguments);
 
-    let VersionChecker = require('ember-cli-version-checker');
-    let checker = new VersionChecker(this);
+    var VersionChecker = require('ember-cli-version-checker');
+    var checker = new VersionChecker(this);
 
     // specifically *not* trying to use `checker.forEmber` because
     // we actually want to see if this is from npm for specific versions
-    let emberSourceDep = checker.for('ember-source', 'npm');
-    let emberCLIDep = checker.for('ember-cli', 'npm');
+    var emberSourceDep = checker.for('ember-source', 'npm');
+    var emberCLIDep = checker.for('ember-cli', 'npm');
 
-    let emberSourceIncludesLegacyShims = emberSourceDep.gt('2.11.0-alpha.0') && emberSourceDep.lt('2.11.0');
-    let emberCLISupportsOverridingShims = emberCLIDep.gt('2.11.0-alpha.0');
+    var emberSourceIncludesLegacyShims = emberSourceDep.gt('2.11.0-alpha.0') && emberSourceDep.lt('2.11.0');
+    var emberCLISupportsOverridingShims = emberCLIDep.gt('2.11.0-alpha.0');
 
     if (!emberCLISupportsOverridingShims) {
       throw new SilentError('To consume ember-cli-shims from npm you must be using ember-cli@2.11.0-beta.1 or greater.  Please update ember-cli to a newer version or remove ember-cli-shims from `package.json`.');
     }
 
-    let projectBowerDeps = this.project.bowerDependencies();
+    var projectBowerDeps = this.project.bowerDependencies();
     if (projectBowerDeps['ember-cli-shims']) {
       throw new SilentError('Using ember-cli-shims as both a bower dependency and an npm dependency is not supported. Please remove `ember-cli-shims` from `bower.json`.');
     }
@@ -33,7 +33,7 @@ module.exports = {
     // their own legacy shims system, so this import is not needed with
     // those ember-source versions
     if (!emberSourceIncludesLegacyShims && emberCLISupportsOverridingShims) {
-      let assetPath = 'vendor/ember-cli-shims/app-shims.js';
+      var assetPath = 'vendor/ember-cli-shims/app-shims.js';
       if (this.import) {
         this.import(assetPath);
       } else {
