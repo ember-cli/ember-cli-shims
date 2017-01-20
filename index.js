53ff464dcb998f8e0b4227a783b4a39a88d3f417
Robert Jackson <rjackson@linkedin.com>
2017-01-20 17:59:28 -0500

    Avoid double import when used with ember-cli versions < 2.11.0-beta.2.

--- index.js
+++ index.js
@@ -12,14 +12,16 @@ module.exports = {
 
     // specifically *not* trying to use `checker.forEmber` because
     // we actually want to see if this is from npm for specific versions
-    let dep = checker.for('ember-source', 'npm');
+    let emberSourceDep = checker.for('ember-source', 'npm');
+    let emberCLIDep = checker.for('ember-cli', 'npm');
 
-    let emberSourceIncludesLegacyShims = dep.gt('2.11.0-alpha.0') && dep.lt('2.11.0');
+    let emberSourceIncludesLegacyShims = emberSourceDep.gt('2.11.0-alpha.0') && emberSourceDep.lt('2.11.0');
+    let emberCLISupportsOverridingShims = emberCLIDep.lt('2.11.0-beta.1');
 
     // ember-source@2.11.0-alpha and 2.11.0-beta series releases included
     // their own legacy shims system, so this import is not needed with
     // those ember-source versions
-    if (!emberSourceIncludesLegacyShims) {
+    if (!emberSourceIncludesLegacyShims && emberCLISupportsOverridingShims) {
       let assetPath = 'vendor/ember-cli-shims/app-shims.js';
       if (this.import) {
         this.import(assetPath);
