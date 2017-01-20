697f6f54befecc21922d7a118739373933c20436
Robert Jackson <rjackson@linkedin.com>
2017-01-20 16:08:25 -0500

    Ensure working (and not duplicating shims) for ember-source@2.11.0-beta.

--- index.js
+++ index.js
@@ -7,11 +7,25 @@ module.exports = {
   included(app) {
     this._super.included.apply(this, arguments);
 
-    let assetPath = 'vendor/ember-cli-shims/app-shims.js';
-    if (this.import) {
-      this.import(assetPath);
-    } else {
-      app.import(assetPath);
+    let VersionChecker = require('ember-cli-version-checker');
+    let checker = new VersionChecker(this);
+
+    // specifically *not* trying to use `checker.forEmber` because
+    // we actually want to see if this is from npm for specific versions
+    let dep = checker.for('ember-source', 'npm');
+
+    let emberSourceIncludesLegacyShims = dep.gt('2.11.0-alpha.0') && dep.lt('2.11.0');
+
+    // ember-source@2.11.0-alpha and 2.11.0-beta series releases included
+    // their own legacy shims system, so this import is not needed with
+    // those ember-source versions
+    if (!emberSourceIncludesLegacyShims) {
+      let assetPath = 'vendor/ember-cli-shims/app-shims.js';
+      if (this.import) {
+        this.import(assetPath);
+      } else {
+        app.import(assetPath);
+      }
     }
   }
 };
