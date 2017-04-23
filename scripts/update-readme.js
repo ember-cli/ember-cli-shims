a827b9cdfadb8d9a20f1e2ef439bb2028b6e292f
Mohammed <m.salahz86@gmailcom>
2017-04-23 22:14:34 +0200

    Fix #1 layout for the API documentation

--- scripts/update-readme.js
+++ scripts/update-readme.js
@@ -52,12 +52,21 @@ Shims provided by this addon
 ----------------------------
 
 <% Object.keys(shims).map((key) => { -%>
-### \`<%= key %>\`
+### <%= key %>
+\`\`\`javascript <%if (Object.keys(shims[key]).includes('default')) { %>
+// <%= shims[key]['default'] %>
+import <%= shims[key]['default'].split(\'.\')[shims[key]['default'].split('.').length-1] %> from \`<%= key %>\`;<% } %>
+<%if (Object.keys(shims[key]).filter(item => item !== 'default').length > 0) { %>import { <%= Object.keys(shims[key]).filter(item => item !== 'default').join(\', \') %> } from \`<%= key %>\`;
+<% } %>\`\`\`
+<%if (Object.keys(shims[key]).filter(item => item !== 'default').length > 0) { %>
+Named exports of \`<%= key %>\`
 
-<% Object.keys(shims[key]).map((exportName) => { -%>
-- \`<%= exportName %>\` <%= shims[key][exportName] %>
+<% Object.keys(shims[key]).filter(item => item !== 'default').map((exportName) => { -%> <%if (shims[key][exportName]) { %> 
+- \`<%= shims[key][exportName].split(\'.\')[shims[key][exportName].split('.').length-1] %>\` <%= shims[key][exportName] %>
+<% } %>
 <% }); -%>
-
+<% } %>
+---
 <% }); -%>
 `;
 
@@ -68,3 +77,15 @@ const renderedShims = ejs.render(template, {
 // @TODO Insert `renderedShims` into README between `<!--[begin-apidocs]-->` and `<!--[end-apidocs]-->`,
 // replacing existing content.
 console.log(renderedShims);
+
+
+// generate md file
+const filepath = 'cheatsheet.md';
+const fs = require('fs');
+fs.openSync(filepath, 'w');
+fs.writeFile(filepath, renderedShims, function(err) {
+  if (err) {
+    return console.log(err);
+  }
+  console.log('The file was saved!');
+});
