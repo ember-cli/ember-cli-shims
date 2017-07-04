bd95c6cc508a8bec7ae6d4a93ad323764954c415
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-07-04 10:16:14 +0200

    Add more import tests

--- tests/unit/general-importing-test.js
+++ tests/unit/general-importing-test.js
@@ -9,4 +9,16 @@ test('ember shim', function(assert) {
 
 test('ember-component', function(assert) {
   assert.equal(self.Ember.Component, require('ember-component').default);
-})
+});
+
+test('ember-runloop', function(assert) {
+  assert.equal(self.Ember.run.debounce, require('ember-runloop').debounce);
+});
+
+test('ember-helper', function(assert) {
+  assert.equal(self.Ember.Helper, require('ember-helper').default);
+});
+
+test('ember-set/ordered', function(assert) {
+  assert.equal(self.Ember.OrderedSet, require('ember-set/ordered').default);
+});
